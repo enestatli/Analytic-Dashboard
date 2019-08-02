49abe884fe949375649b0a998ceb0385fe2d7549
enestatli <eenestatli@gmail.com>
2019-08-02 20:07:15 +0300

    Updates

---
+++ precache-manifest.ba0250cff45970e09057f5a18e733f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155782ba0bc6f4be6f0c330e746900b8",
+    "url": "/dashboard-frontend/index.html"
+  },
+  {
+    "revision": "54a48a218da41d30ace4",
+    "url": "/dashboard-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f841771251e398e22be6",
+    "url": "/dashboard-frontend/static/css/main.961f9ea9.chunk.css"
+  },
+  {
+    "revision": "54a48a218da41d30ace4",
+    "url": "/dashboard-frontend/static/js/2.4b675971.chunk.js"
+  },
+  {
+    "revision": "f841771251e398e22be6",
+    "url": "/dashboard-frontend/static/js/main.48fe4f01.chunk.js"
+  },
+  {
+    "revision": "d683423eb510768d4106",
+    "url": "/dashboard-frontend/static/js/runtime~main.977243bb.js"
+  }
+]);
